d848f1afc86025ca4b98bd7a519e81f24ca009bc
dharani-dp-33 <dharanivendhan143@gmail.com>
2025-03-26 19:48:47 +0530

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -12,13 +12,21 @@ echo noop > /sys/block/mmcblk0/queue/scheduler
 echo 10 > /proc/sys/vm/swappiness
 echo 20 > /proc/sys/vm/dirty_ratio
 
-# Thermal management settings
-# Reduce maximum CPU frequency to manage heat
-for cpu in /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq; do
-    echo 1500000 > $cpu
+# Set CPU frequencies based on Realme X7 Max specifications
+# Cortex-A78 core at 3.0 GHz
+echo 3000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+
+# Cortex-A78 cores at 2.6 GHz
+for cpu in /sys/devices/system/cpu/cpu[1-3]/cpufreq/scaling_max_freq; do
+    echo 2600000 > $cpu
+done
+
+# Cortex-A55 cores at 2.0 GHz
+for cpu in /sys/devices/system/cpu/cpu[4-7]/cpufreq/scaling_max_freq; do
+    echo 2000000 > $cpu
 done
 
 # Enable thermal throttling
 echo 1 > /sys/devices/system/cpu/cpu*/cpufreq/thermal_throttle_enabled
 
-# Additional thermal management tweaks can be added here
+# Additional performance and thermal management tweaks can be added here
