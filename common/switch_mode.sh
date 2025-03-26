dc407f8102f2d7759f83f5ba822cfb455659cc01
dharani-dp-33 <dharanivendhan143@gmail.com>
2025-03-26 20:15:20 +0530

    Update switch_mode.sh

--- common/switch_mode.sh
+++ common/switch_mode.sh
@@ -2,10 +2,14 @@
 
 if [ "$1" = "performance" ]; then
     sh /common/performance.sh
-    echo "Switched to performance mode"
+    sh /common/boost.sh
+    echo "Switched to performance mode and boosted: All non-essential background and active apps have been closed."
 elif [ "$1" = "battery" ]; then
     sh /common/battery.sh
     echo "Switched to battery-saving mode"
+elif [ "$1" = "boost" ]; then
+    sh /common/boost.sh
+    echo "Boost mode activated: All non-essential background and active apps have been closed."
 else
-    echo "Usage: switch_mode.sh [performance|battery]"
+    echo "Usage: switch_mode.sh [performance|battery|boost]"
 fi
