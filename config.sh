306ec4d62993930f03f5c9c96c2a212524aa5dfe
dharani-dp-33 <dharanivendhan143@gmail.com>
2025-03-26 20:32:14 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,29 @@
-#!/system/bin/sh
-SKIPMOUNT=false
+##########################################################################################
+#
+# Configuration for the Magisk module
+#
+##########################################################################################
+
+# Set the module name
+MODID=your_module_id
+
+# Set the module version
+VERSION=1.0
+
+# Set the module version code
+VERSIONCODE=100
+
+# Set the module author
+AUTHOR="Your Name"
+
+# Set the module description
+DESCRIPTION="Magisk module to switch between performance, battery, and boost modes."
+
+# Define installation variables
+AUTOMOUNT=true
 PROPFILE=false
 POSTFSDATA=false
 LATESTARTSERVICE=true
+
+# Import functions
+. $MODPATH/common/service.sh
